e612897b9c7cc13b372a2349b415ebcd0f75608b
jake.rowland <jakerowland@verizon.net>
2018-09-27 10:07:56 -0500

    file handler

---
+++ utils/FileHandler.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   FileHandler.cpp
+ * Author: jake.rowland
+ * 
+ * Created on September 27, 2018, 10:01 AM
+ */
+
+#include "FileHandler.h"
+
+FileHandler::FileHandler() {
+}
+
+FileHandler::FileHandler(const FileHandler& orig) {
+}
+
+FileHandler::~FileHandler() {
+}
+
---
+++ utils/FileHandler.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   FileHandler.h
+ * Author: jake.rowland
+ *
+ * Created on September 27, 2018, 10:01 AM
+ */
+
+#ifndef FILEHANDLER_H
+#define FILEHANDLER_H
+
+class FileHandler {
+public:
+    FileHandler();
+    FileHandler(const FileHandler& orig);
+    virtual ~FileHandler();
+private:
+
+};
+
+#endif /* FILEHANDLER_H */
+
